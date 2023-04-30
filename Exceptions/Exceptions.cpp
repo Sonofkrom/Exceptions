f4a7f77a4941ace813021d876e14f5d1f622fa0c
Marco <Marco@CLANK>
2023-04-29 20:24:41 -0400

    Add project files.

---
+++ Exceptions/Exceptions.cpp
@@ -0,0 +1,16 @@
+// Marcus Needham
+// CIS 1202 501
+// April 29 2023
+
+#include <iostream>
+
+char character(char start, int offset);
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+char character(char start, int offset) {
+    return 'a';
+}
