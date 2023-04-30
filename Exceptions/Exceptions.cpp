d2fdd1ff11e72cf07148329d9d0183862cba7d16
Marco <Marco@CLANK>
2023-04-29 21:14:57 -0400

    Completed (I think) working code, will now create driver.

--- Exceptions/Exceptions.cpp
+++ Exceptions/Exceptions.cpp
@@ -4,27 +4,42 @@
 
 #include <iostream>
 
+using namespace std;
+
+class invalidCharacterException : public exception {
+};
+
+class invalidRangeException : public exception {
+};
+
 char character(char start, int offset);
 
 int main()
 {
     try {
-        character('a', -1);
+        cout << character('a', 1);
     }
-    catch(invalidRangeException){
-
+    catch(invalidRangeException x){
+        cout << "Invalid Range!\n";
     }
-    catch (invalidCharacterException) {
-
+    catch (invalidCharacterException x) {
+        cout << "Invalid Character!\n";
     }
 }
 
-class invalidCharacterException {
-};
-
-class invalidRangeException {
-};
-
 char character(char start, int offset) {
-    return 'a';
+    if (start > 96 && start < 123) {
+        int test = start + offset;
+        if (test <= 96 || test >= 123)
+            throw invalidRangeException();
+        return char(test);
+    }
+    else if (start > 64 && start < 91) {
+        int test = start + offset;
+        if (test <= 64 || test >= 91)
+            throw invalidRangeException();
+        return char(test);
+    }
+    else
+        throw invalidCharacterException();
 }
